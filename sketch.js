9273bb659946a6a1208d0690843697fb7447fdd3
charcoallake <cjd3549@gmail.com>
2020-05-23 19:41:45 +0900

    0.0
    basic declaration

---
+++ sketch.js
@@ -0,0 +1,176 @@
+/*
+게임판 관련 변수 (board)
+보드에 접근   : board[y][x]. (x, y 가 아닌 y, x 임에 주의!)
+
+보드의 칸의 ownership 은 숫자로 대표되도록 함. 예를 들어서 (i, j) 좌표의 보드의 owner 가 player1 이라면 board[j][i] = 1;
+owner 가 없을 경우 board[i][j] = 0; (default)
+
+<모듈>
+display_board() : 보드를 화면에 표시, ownership 에 따라 다른 색 또는 건물로 각 cell 이 나타나야 함.
+
+*/
+let board = [];       // 게임판
+let board_size = 10;  // default size, 게임판 기본 크기
+let cell_size = 50;   // 셀 하나 가로 크기
+
+/*
+플레이어 오브젝트 => class Marker
+플레이어 배열 => player
+플레이어 점수판 => player_score
+
+플레이어 선택은 index를 사용하도록 함.
+예를 들어서 player1 은 player[1], player2 는 player[2].
+player[0] 는 만일의 목적을 위해 만들고 비워두도록 할게요!
+
+플레이어 점수판 참조 역시 index 를 사용.
+예를 들어서 player1 의 점수는 player_score[1].
+점수판에는 플레이어가 차지한 칸의 갯수가 기록됨.
+score 값에 따라 승리 여부 결정.
+
+<모듈>
+move() : key input 에 따라 player를 움직임
+display() : 움직인 player 를 화면에 그림.
+...
+
+*/
+let player_num = 2;
+let player = [];
+let player_score = [];
+
+
+/*
+주사위 관련 변수 (dice)
+*/
+let dice;             // 1~6, roll_dice 결과값
+let dice_button;      // 테스트 버튼. dice_button 을 누르면 roll_dice 가 실행된다고 가정합니다.
+
+/*
+게임 오버 관련
+*/
+let game_over = false;  // game_over flag. 만약 보드 전체가 ownership 을 갖게 되면 game_over = true;
+
+/*
+화면 rotation slider, display 함수 등 visual 만들때 사용하세요
+*/
+let rotX, rotY, rotZ;
+
+function preload(){
+  // 나중에 모델 로드, 텍스처 로드
+}
+
+function setup(){
+
+  cnv = createCanvas(windowWidth, windowHeight, WEBGL);
+  cnv.position(0,0);
+  
+  // board 2차원 배열 생성
+  for(let i=1; i<=board_size; i++){
+        board[i] = [];
+        for(let j=1; j<=board_size; j++){
+          // (j, i) 번째 칸 생성*
+          // owner 가 없는 칸으로 초기화
+          board[i][j] = 0;
+        }
+  }
+
+  player[0] = new Marker(0, 0, 0);  // 임시 플레이어
+  player[1] = new Marker(1, 1, 1);  // player1, default position (1, 1)
+  player[2] = new Marker(2, board_size, board_size); // player 2, default position (10, 10)
+
+
+  // sliders
+  rotX = createSlider(0, 180, 75);
+  rotY = createSlider(0, 180, 0);
+  rotZ = createSlider(0, 180, 0);
+  rotX.position(10,10);
+  rotY.position(10,30);
+  rotZ.position(10,50);
+
+  dice_button = createButton('roll');
+  dice_button.mousePressed(roll_dice);
+  dice_button.position(10, 70);
+}
+
+
+function draw(){
+
+  background(200);
+  rotateX(radians(rotX.value()));
+  rotateY(radians(rotY.value()));
+  rotateZ(radians(rotZ.value()));
+  
+  if(!game_over){
+
+    display_board();
+    display_dice();
+
+    for(let i=1; i<=player_num; i++){
+      player[i].display();
+    }
+
+    plane(1000, 1000);
+    check_gameover();
+  }
+
+} // end of draw
+
+
+function display_board(){
+
+  // 보드를 화면에 표시
+  // 위치를 잡고, 보드 배열의 값에 따라(owner에 따라) 색이 바뀌거나 모델이 나타남.
+
+    for(let i=1; i<= board_size; i++){
+      for(let j=1; j<=board_size; j++){
+        push();
+        translate(-width/2+j*cell_size, -height/2+i*cell_size,  cell_size/2);  // 임의로 위치 잡았는데 위치 제대로 잡는건 담당하시는 분이 해주세요!
+        box(cell_size, cell_size, cell_size);
+        pop();
+      }
+    }
+
+ } // end of display_board
+
+
+
+class Marker{
+
+  constructor(n, x, y){
+    // n : player number
+    // x, y :default position
+    this.n = n;
+    this.x = x;
+    this.y = y;
+  }
+
+  move(){
+    // key input 에 따라 Marker 의 좌표값을 바꿈
+    // 바뀐 좌표상의 board 의 ownership 을 바꿈
+    // player_score 를 업데이트 함. (만약 player1 이 player2 의 칸을 먹었다면 두 플레이어의 스코어가 모두 변해야 해요!)
+  }
+
+  display(){
+    // Marker 를 보드 위에 표시.
+    // 처음에는 sphere 같은 것으로 하고 나중에 model 씌우면 될 것 같습니다.
+    push();
+    translate(-width/2+this.x*cell_size, -height/2+this.y*cell_size,  cell_size);  // 임의로 위치 잡았는데 위치 제대로 잡는건 담당하시는 분이 해주세요!
+    sphere(30);
+    pop();
+  }
+
+}
+
+function roll_dice(){
+  // 주사위를 굴림
+  // 굴린 주사위 값이 dice_value 에 저장
+  // 주사위를 굴릴 때마다 턴이 바뀜. player1 => player2 => player1 ...
+} // end of roll_dice
+
+function display_dice(){
+  // 주사위를 화면에 표시
+} // end of display_dice
+
+function check_gameover(){
+  // 게임오버인지 체크
+  // 만약 게임오버 조건을 만족하게 되면 game_over = true; 로 바꾸어 줍니다.
+}
