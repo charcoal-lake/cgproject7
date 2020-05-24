102272537cc0388b3ffc8a33a0f131ef491ad061
charcoallake <cjd3549@gmail.com>
2020-05-24 14:57:16 +0900

    예린
    align change

--- sketch.js
+++ sketch.js
@@ -224,14 +224,14 @@ function keyPressed(){
 
 function createUI(){
 
-  let ui_title = createDiv('Board Game');
+  let ui_title = createDiv('Board Game').size(200, 10);
   ui_title.position(20, 20);
   ui_current_dice = createDiv('Current Dice : ');
-  ui_current_dice.position(50, 200);
+  ui_current_dice.position(50, 200).size(200, 10);
   ui_player1_score = createDiv('<b>Score</b> Player1 : ' + player_score[1]);
   ui_player2_score = createDiv('<b>Score</b> Player2 : ' + player_score[2]);
-  ui_player1_score.position(50, 220);
-  ui_player2_score.position(50, 240);
+  ui_player1_score.position(50, 220).size(200, 10);
+  ui_player2_score.position(50, 240).size(200, 10);
   
 }
 
